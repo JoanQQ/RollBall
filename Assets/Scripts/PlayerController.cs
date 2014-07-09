d04b90d88163b4137bfc43cec70a3ad45794fcd7
Phyllis Zhao <phyllis@happylatte.com>
2014-07-09 16:56:29 +0800

    second commit for git integration with jenkins on the unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate() {
+		float moveHorizaontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizaontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+		}
+	}
+}
+
+// Destroy(other.gameObject);
+// gameObject.tag == "Player";
+// gameObject.SetActive(false);
